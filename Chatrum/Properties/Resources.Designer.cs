2e1e11108738cf22c2816e6b7b7d9b9c89f144b1
Kresten Laust <kressckerl@gmail.com>
2021-12-10 16:49:56 +0100

    typisk

--- Chatrum/Properties/Resources.Designer.cs
+++ Chatrum/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Chatrum.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap PendingMessageIcon {
+            get {
+                object obj = ResourceManager.GetObject("PendingMessageIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
